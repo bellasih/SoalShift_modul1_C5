b0045d6f6f1786936e70beeb1501fbda25f7c6d2
bellasih <43091473+bellasih@users.noreply.github.com>
2019-02-22 13:13:32 +0700

    Update soal1.sh

--- soal1.sh
+++ soal1.sh
@@ -6,7 +6,7 @@ unzip /home/bella/soalshift1/nature.zip
 
 i=0
 
-for n in /home/bella/soalshift1/nature/*.jpg;
+for file_jpg in /home/bella/soalshift1/nature/*.jpg;
 do
     base64 --decode $n | xxd -r > /home/bella/soalshift1/hasil_decrypt_zip/$i.jpg
     i=$(($i+1))

f02197851e15bb6dc9da01b4ba6fc47c4b439455
bellasih <43091473+bellasih@users.noreply.github.com>
2019-02-22 13:38:59 +0700

    Update soal5.sh

--- soal5.sh
+++ soal5.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-awk '/cron/ || /CRON/,!/sudo/' /var/log/syslog | awk 'NF < 13' >> /home/bella/soalshift1/hasil5.log
+awk '/cron/ || /CRON/,!/sudo/' /var/log/syslog | awk 'NF < 13' >> /home/bella/modul1/hasil5.log

6e85f80a636fa970791694fb58dfedc7b580255b
bellasih <43091473+bellasih@users.noreply.github.com>
2019-02-22 12:50:16 +0700

    Update soal4_dh.sh

--- soal4_dh.sh
+++ soal4_dh.sh
@@ -5,7 +5,7 @@ hr=${date_time:0:2}
 
 lc="abcdefghijklmnopqrstuvwxyz"
 uc="ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-lc+=$kcl
-uc+=$bsr
+lc+=$lc
+uc+=$uc
 
 cat "$date_time" | tr "${lc:$hr:26}${uc:$hr:26}" "${lc:0:26}${uc:0:26}" | cat > "/home/bella/soalshift1/hasil_decrypt $date_time"

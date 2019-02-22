caf2fb80e945f9a1570baf57f29b70a4fd062465
bellasih <43091473+bellasih@users.noreply.github.com>
2019-02-22 12:55:20 +0700

    Update soal4.sh

--- soal4.sh
+++ soal4.sh
@@ -5,6 +5,6 @@ filename=`date +"%H:%M %d-%m-%y"`
 lc="abcdefghijklmnopqrstuvwxyz"
 uc="ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 lc+=$lc
-uc+=$buc
+uc+=$uc
 
 cat /var/log/syslog | tr "${lc:0:26}${uc:0:26}" "${lc:$hr:26}${uc:$hr:26}" > "$filename"
